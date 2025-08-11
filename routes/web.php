264181fb8ba296524780e3868cfe46b09d7d5115
Yogisswara <swarayogi363@gmail.com>
2025-08-11 15:05:35 +0700

    add cuti

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\EventAttendanceController;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\SalaryController;
+use App\Http\Controllers\CutiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,7 +47,7 @@ Route::middleware('auth')->group(function () {
 // Admin Routes
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-    
+
     // Client Routes
     Route::get('/clients', [ClientController::class, 'index'])->name('client.index');
     Route::get('/clients/create', [ClientController::class, 'create'])->name('client.create');
@@ -55,7 +56,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/clients/{id}/edit', [ClientController::class, 'edit'])->name('client.edit');
     Route::put('/clients/{id}', [ClientController::class, 'update'])->name('client.update');
     Route::delete('/clients/{id}', [ClientController::class, 'destroy'])->name('client.destroy');
-    
+
     // Project Routes
     Route::get('/projects', [ProjectController::class, 'index'])->name('project.index');
     Route::get('/projects/create', [ProjectController::class, 'create'])->name('project.create');
@@ -64,7 +65,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/projects/{id}/edit', [ProjectController::class, 'edit'])->name('project.edit');
     Route::put('/projects/{id}', [ProjectController::class, 'update'])->name('project.update');
     Route::delete('/projects/{id}', [ProjectController::class, 'destroy'])->name('project.destroy');
-    
+
     // Invoice Routes
     Route::get('/invoices', [InvoiceController::class, 'index'])->name('invoice.index');
     Route::get('/invoices/mpa', [InvoiceController::class, 'mpa'])->name('invoice.mpa');
@@ -74,7 +75,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/invoices/{id}/edit', [InvoiceController::class, 'edit'])->name('invoice.edit');
     Route::put('/invoices/{id}', [InvoiceController::class, 'update'])->name('invoice.update');
     Route::delete('/invoices/{id}', [InvoiceController::class, 'destroy'])->name('invoice.destroy');
-    
+
     // User Management Routes
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
     Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
@@ -83,7 +84,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
     Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
     Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-    
+
     // Task Management Routes
     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
     Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
@@ -92,7 +93,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
     Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
     Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-    
+
     // Calendar Routes
     Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index');
     Route::get('/calendar/create', [CalendarController::class, 'create'])->name('calendar.create');
@@ -100,16 +101,16 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/calendar/{id}/edit', [CalendarController::class, 'edit'])->name('calendar.edit');
     Route::put('/calendar/{id}', [CalendarController::class, 'update'])->name('calendar.update');
     Route::delete('/calendar/{id}', [CalendarController::class, 'destroy'])->name('calendar.destroy');
-    
+
     // Reports Routes
     Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
-    
+
     // Settings Routes
     Route::get('/settings', [SettingController::class, 'index'])->name('settings.index');
     Route::post('/settings', [SettingController::class, 'update'])->name('settings.update');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 Route::get('/', function () {
     return view('welcome');
@@ -130,7 +131,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
     Route::post('/events', [EventController::class, 'store'])->name('events.store');
     Route::patch('/events/{event}/status', [EventController::class, 'updateStatus'])->name('events.updateStatus');
-
 });
 Route::middleware(['auth'])->group(function () {
     Route::resource('attendances', AttendanceController::class)
@@ -142,4 +142,10 @@ Route::post('/attendances/proses', [AttendanceController::class, 'proses'])->nam
 Route::get('/salary', [SalaryController::class, 'index'])->name('salary.index');
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/pengajuan-cuti', [CutiController::class, 'index'])->name('cuti.index');
+    Route::post('/pengajuan-cuti', [CutiController::class, 'store'])->name('cuti.store');
+    Route::get('/pengajuan-cuti/create', [CutiController::class, 'create'])->name('cuti.create');
+});
