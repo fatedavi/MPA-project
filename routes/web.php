6b965353e0a35cbf362d68144ebe1f40b8d13858
Yogisswara <swarayogi363@gmail.com>
2025-08-12 15:36:41 +0700

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -77,14 +77,16 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::put('/invoices/{id}', [InvoiceController::class, 'update'])->name('invoice.update');
     Route::delete('/invoices/{id}', [InvoiceController::class, 'destroy'])->name('invoice.destroy');
 
-    // User Management Routes
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/users', [UserController::class, 'store'])->name('users.store');
-    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::middleware(['auth', RoleMiddleware::class . ':admin,super_admin'])->group(function () {
+        // User Management Routes
+        Route::get('/users', [UserController::class, 'index'])->name('users.index');
+        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+        Route::post('/users', [UserController::class, 'store'])->name('users.store');
+        Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+        Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+        Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+        Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    });
 
     // Task Management Routes
     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
@@ -126,58 +128,52 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::middleware(['auth'])->group(function () {
+/*
+|--------------------------------------------------------------------------
+| ADMIN & SUPER_ADMIN
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth', RoleMiddleware::class . ':admin,super_admin'])->group(function () {
 
+    // Event
     Route::get('/events', [EventController::class, 'index'])->name('events.index');
     Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
     Route::post('/events', [EventController::class, 'store'])->name('events.store');
     Route::patch('/events/{event}/status', [EventController::class, 'updateStatus'])->name('events.updateStatus');
 
-    Route::patch('/events/{event}/status', [EventController::class, 'updateStatus'])->name('events.updateStatus');
-});
-Route::middleware(['auth'])->group(function () {
-    Route::resource('attendances', AttendanceController::class)
-        ->only(['index', 'create', 'store', 'edit', 'update']);
-});
-// web.php
-Route::get('/attendances', [AttendanceController::class, 'index'])->name('attendances.index');
-Route::post('/attendances/proses', [AttendanceController::class, 'proses'])->name('attendance.proses');
-Route::get('/salary/save', [SalaryController::class, 'saveSalaries'])->name('salary.save');
-Route::resource('salary', SalaryController::class)->only(['index']);
-Route::get('/salary/history', [SalaryController::class, 'history'])->name('salary.history');
+    // Users (contoh kalau ada)
+    // Route::resource('users', UserController::class);
 
+    // Gaji
+    Route::get('/salary/save', [SalaryController::class, 'saveSalaries'])->name('salary.save');
+    Route::get('/salary/history', [SalaryController::class, 'history'])->name('salary.history');
+    Route::resource('salary', SalaryController::class)->only(['index']);
 
-
-Route::middleware(['auth', RoleMiddleware::class . ':admin,super_admin'])->group(function () {
-    Route::resource('employees', EmployeeController::class);
-});
-
-require __DIR__ . '/auth.php';
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/pengajuan-cuti', [CutiController::class, 'index'])->name('cuti.index');
-    Route::post('/pengajuan-cuti', [CutiController::class, 'store'])->name('cuti.store');
-    Route::get('/pengajuan-cuti/create', [CutiController::class, 'create'])->name('cuti.create');
-    Route::resource('attendances', AttendanceController::class)
-        ->only(['index', 'create', 'store', 'edit', 'update']);
-});
-
-Route::get('/event-attendances', [EventAttendanceController::class, 'index'])->name('event-attendances.index');
-Route::get('/event-attendances/create', [EventAttendanceController::class, 'create'])->name('event-attendances.create');
-Route::post('/event-attendances', [EventAttendanceController::class, 'store'])->name('event-attendances.store');
-Route::get('/event-attendances/{id}/edit', [EventAttendanceController::class, 'edit'])->name('event-attendances.edit');
-Route::put('/event-attendances/{id}', [EventAttendanceController::class, 'update'])->name('event-attendances.update');
-Route::delete('/event-attendances/{id}', [EventAttendanceController::class, 'destroy'])->name('event-attendances.destroy');
-Route::resource('event-attendances', EventAttendanceController::class);
-
-
-Route::middleware(['auth'])->group(function () {
+    // Daftar cuti admin
     Route::get('/daftarcuti-admin', [CutiController::class, 'adminIndex'])->name('cuti.admin.index');
     Route::patch('/cuti/{id}/approve', [CutiController::class, 'approve'])->name('cuti.approve');
     Route::patch('/cuti/{id}/reject', [CutiController::class, 'reject'])->name('cuti.reject');
+
+    // Employees
+    Route::resource('employees', EmployeeController::class);
+
+    // Event Attendance
+    Route::resource('event-attendances', EventAttendanceController::class);
 });
 
-Route::get('/events', [EventController::class, 'index'])->name('events.index');
-Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
-Route::post('/events', [EventController::class, 'store'])->name('events.store');
-Route::patch('/events/{event}/status', [EventController::class, 'updateStatus'])->name('events.updateStatus');
+/*
+|--------------------------------------------------------------------------
+| KARYAWAN
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth', RoleMiddleware::class . ':karyawan'])->group(function () {
+
+    // Pengajuan cuti
+    Route::get('/pengajuan-cuti', [CutiController::class, 'index'])->name('cuti.index');
+    Route::post('/pengajuan-cuti', [CutiController::class, 'store'])->name('cuti.store');
+    Route::get('/pengajuan-cuti/create', [CutiController::class, 'create'])->name('cuti.create');
+
+    // Absen
+    Route::resource('attendances', AttendanceController::class)->only(['index', 'create', 'store', 'edit', 'update']);
+    Route::post('/attendances/proses', [AttendanceController::class, 'proses'])->name('attendance.proses');
+});
