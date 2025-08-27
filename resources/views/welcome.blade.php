bba00ab57d6096b9d6e2f8c33e18c4a7f1ded3a1
Yogisswara <swarayogi363@gmail.com>
2025-08-27 14:52:31 +0700

    change logo web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <!-- Favicon -->
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <!-- Alternatif PNG -->
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/mpa_logo.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/mpalogo.png') }}">
 
     <!-- Fonts & Icons -->
     <link rel="preconnect" href="https://fonts.bunny.net">
